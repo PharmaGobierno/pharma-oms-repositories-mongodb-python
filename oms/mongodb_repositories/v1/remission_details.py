a4ad38b4ecbc7a49fa097764adbaa0b008a93b1d
Ariel Villeda <ariel.villeda@cargamos.com>
2025-09-02 16:22:22 -0600

    remission-details repository

---
+++ oms/mongodb_repositories/v1/remission_details.py
@@ -0,0 +1,26 @@
+from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
+
+from .base import BaseMongoDbRepository
+
+
+class RemissionDetailsRepository(BaseMongoDbRepository):
+    def get_by_tracking_id(
+        self,
+        tracking_id: str,
+        *,
+        tenant: Optional[List[str]] = None,
+        sort: Optional[List[Tuple[str, int]]] = None,
+        projection: Optional[Union[list, dict]] = None,
+        limit: Optional[int] = None,
+    ) -> Tuple[int, Iterator[dict]]:
+        filter: Dict[str, Any] = {"remission.tracking_id": tracking_id}
+        if tenant:
+            filter.update({"tenant_id": {"$in": tenant}})
+        documents_count: int = self._collection.count_documents(filter)
+        documents_cursor = self._collection.find(
+            filter,
+            sort=sort,
+            projection=projection,
+            limit=(limit or self.DEFAULT_QUERY_LIMIT),
+        )
+        return documents_count, map(lambda item: item, documents_cursor)
